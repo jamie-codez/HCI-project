0b31fdffe2bee93d374d9a72ca5ed2456f35f2f2
Jaimie <cruiseomondi90@gmai.com>
2021-06-07 19:26:24 +0300

    initial commit

---
+++ Backend/web-api/src/test/java/com/code/softech/backend/api/webapi/WebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.softech.backend.api.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
